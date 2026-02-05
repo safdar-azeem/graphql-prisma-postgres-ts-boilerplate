1a215b59934391a71d61e13be462c13ada30e4dd
safdar-azeem <safdarazeem00@gmail.com>
2026-02-06 03:23:18 +0500

    Update prisma.ts

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '@/generated/prisma'
+import { PrismaClient } from '@prisma/client'
 import { PrismaSharding } from 'prisma-sharding'
 
 const connectionString = `${process.env.DATABASE_URL}`
