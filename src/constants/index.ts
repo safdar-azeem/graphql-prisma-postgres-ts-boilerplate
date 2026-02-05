aa7580560f17f68a6831cec86090efc98f7bc810
safdar-azeem <safdarazeem00@gmail.com>
2026-02-05 16:44:44 +0500

    Add index.ts

---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+export const NODE_ENV = process.env.NODE_ENV || 'development'
+
+export const IS_PRODUCTION = NODE_ENV === 'production'
+export const IS_DEVELOPMENT = NODE_ENV === 'development'
+
+export const JWT_EXPIRES_IN_SECONDS = parseInt(process.env.JWT_EXPIRES_IN_SECONDS || '604800', 10)
+
+export const JWT_SECRET = process.env.JWT_SECRET as string
+export const APP_NAME = 'Builto'
+export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000'
+
+export const STORAGE_SERVICE_URL = process.env.STORAGE_SERVICE_URL || 'http://localhost:4001'
