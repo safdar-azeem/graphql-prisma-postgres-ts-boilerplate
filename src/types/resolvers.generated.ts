0be4e1ba3d64e152b71fed188d56edcf56392df1
safdar-azeem <safdarazeem00@gmail.com>
2026-02-05 16:44:46 +0500

    Add resolvers.generated.ts

---
+++ src/types/resolvers.generated.ts
@@ -0,0 +1,7 @@
+/* This file was automatically generated. DO NOT UPDATE MANUALLY. */
+import type { Resolvers } from './types.generated'
+import { DateTimeResolver, JSONResolver } from 'graphql-scalars'
+export const resolvers: Resolvers = {
+  DateTime: DateTimeResolver,
+  JSON: JSONResolver,
+}
