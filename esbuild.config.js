f7d7eb135edb1bf8e61599b922eff0e5107c70aa
safdar-azeem <safdarazeem00@gmail.com>
2026-02-06 01:54:47 +0500

    Update esbuild.config.js

--- esbuild.config.js
+++ esbuild.config.js
@@ -4,22 +4,12 @@ esbuild
   .build({
     entryPoints: ['src/server.ts'],
     bundle: true,
-    outfile: 'dist/index.js',
+    outfile: 'dist/index.cjs', // Use .cjs extension for CommonJS
     platform: 'node',
-    format: 'esm', // Force ESM output
+    // format: 'cjs', // Default is cjs
     sourcemap: true,
     minify: true,
-    external: ['bcrypt', 'sharp', '@prisma/client', 'prisma-sharding', 'ioredis'], // Exclude native modules
-    banner: {
-      js: `
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-`,
-    },
+    external: ['bcrypt', 'sharp', '@prisma/client', 'prisma-sharding', 'ioredis'],
     loader: {
       '.tsx': 'tsx',
       '.ts': 'ts',
