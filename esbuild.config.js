ad7e2d295495751eed6cba06ad761b2a787e3401
safdar-azeem <safdarazeem00@gmail.com>
2026-02-05 16:44:39 +0500

    Add esbuild.config.js

---
+++ esbuild.config.js
@@ -0,0 +1,16 @@
+import esbuild from 'esbuild'
+
+esbuild
+  .build({
+    entryPoints: ['src/server.ts'],
+    bundle: true,
+    outfile: 'dist/index.js',
+    platform: 'node',
+    sourcemap: true,
+    minify: true,
+    loader: {
+      '.tsx': 'tsx',
+      '.ts': 'ts',
+    },
+  })
+  .catch(() => process.exit(1))
