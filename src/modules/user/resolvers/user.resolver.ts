f9713aa53f814b8ff8bb5e0887c0501cb24979c7
safdar-azeem <safdarazeem00@gmail.com>
2026-02-09 15:44:37 +0500

    Update user.resolver.ts

--- src/modules/user/resolvers/user.resolver.ts
+++ src/modules/user/resolvers/user.resolver.ts
@@ -1,11 +1,27 @@
 import { requireAuth } from '@/guards'
 import { Context } from '@/types/context.type'
 import { Resolvers } from '@/types/types.generated'
+import { cache } from '@/cache'
 
 export const userResolver: Resolvers<Context> = {
   Query: {
     me: requireAuth(async (_parent, _args, { user }) => {
       return user
+    }),
+  },
+  Mutation: {
+    updateUserProfile: requireAuth(async (_parent, { data }, { user, client }) => {
+      const updatedUser = await client.user.update({
+        where: { id: user.id },
+        data: {
+          username: data.username ?? undefined,
+          avatar: data.avatar ?? undefined,
+        },
+      })
+
+      await cache.invalidateUser(user.id)
+
+      return updatedUser
     }),
   },
 }
