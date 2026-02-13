4a3ece58b8f97b9dbb36176003b94adc53109eb4
safdar-azeem <safdarazeem00@gmail.com>
2026-02-13 17:15:56 +0500

    Update index.ts

--- src/modules/auth/index.ts
+++ src/modules/auth/index.ts
@@ -1,6 +1,7 @@
 import { mergeResolvers } from '@graphql-tools/merge'
 import { authResolver } from './resolvers/auth.resolver'
 import { twoFaResolvers } from './resolvers/2fa.resolver'
+import { tokenResolver } from './resolvers/token.resolver'
 
-export const authResolvers = mergeResolvers([authResolver, twoFaResolvers])
+export const authResolvers = mergeResolvers([authResolver, twoFaResolvers, tokenResolver])
 export * from './utils/auth.utils'
