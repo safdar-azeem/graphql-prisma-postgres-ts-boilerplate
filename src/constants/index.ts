60f8f014225b5f70f91b00ab49804286b66f3c1a
safdar-azeem <safdarazeem00@gmail.com>
2026-02-13 17:15:56 +0500

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,7 +5,8 @@ export const IS_DEVELOPMENT = NODE_ENV === 'development'
 export const ENABLE_LOGER = false
 
 export const JWT_EXPIRES_IN_SECONDS = parseInt(process.env.JWT_EXPIRES_IN_SECONDS || '604800', 10)
-
+export const ACCESS_TOKEN_EXPIRES_IN = 15 * 60 // 15 minutes
+export const REFRESH_TOKEN_EXPIRES_IN = 7 * 24 * 60 * 60 // 7 days
 export const JWT_SECRET = process.env.JWT_SECRET as string
 export const APP_NAME = 'Builto'
 export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000'
