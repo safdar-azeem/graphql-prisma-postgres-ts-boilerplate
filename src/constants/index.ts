7ff242388eedad5061c4c1bca1ca0b00e27835a2
safdar-azeem <safdarazeem00@gmail.com>
2026-02-13 18:52:23 +0500

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -9,7 +9,7 @@ export const ACCESS_TOKEN_EXPIRES_IN = 15 * 60 // 15 minutes
 export const REFRESH_TOKEN_EXPIRES_IN = 7 * 24 * 60 * 60 // 7 days
 export const JWT_SECRET = process.env.JWT_SECRET as string
 export const APP_NAME = 'Builto'
-export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000'
+export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:4202'
 export const CORS_ALLOWED_ORIGINS = process.env.CORS_ALLOWED_ORIGINS || ''
 
 export const STORAGE_SERVICE_URL = process.env.STORAGE_SERVICE_URL || 'http://localhost:4201'
