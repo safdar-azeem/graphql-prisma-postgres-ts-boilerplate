667e6506bb994ac844b73abdb17e48667c1225d7
safdar-azeem <safdarazeem00@gmail.com>
2026-03-11 16:23:30 +0500

    Update index.ts

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -3,5 +3,14 @@ import { scalars } from '@/graphql/scalars'
 import { authResolvers } from './auth'
 import { userResolver } from './user'
 import { uploadResolver } from './upload'
+import { roleResolver } from './role'
+import { userManagementResolver } from './user-management'
 
-export const resolvers = mergeResolvers([scalars, authResolvers, userResolver, uploadResolver])
+export const resolvers = mergeResolvers([
+  scalars,
+  authResolvers,
+  userResolver,
+  uploadResolver,
+  roleResolver,
+  userManagementResolver,
+])
